0dfc2b128708f4a78f71ff480aaac6958df0a23b
Howard Lau <howardlau1999@hotmail.com>
2020-02-23 02:51:44 +0800

    add: nexusphp hash for compatibility

---
+++ app/Providers/NexusPhpHashServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class NexusPhpHashServiceProvider extends ServiceProvider {
+
+    public function register() {
+        //
+    }
+
+    public function boot() {
+        $this->app->make('hash')->extend('nexusphp', function () {
+            return new \App\Helpers\NexusPhpHash\NexusPhpHasher;
+        });
+    }
+}
